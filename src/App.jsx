f7e462d3aff0ac840e7d2a9a7a3cd79738e8f4fe
emilianojduarte <emilianojduarte@gmail.com>
2024-07-28 15:07:45 -0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
+import PageNotFound from "./pages/PageNotFound";
+import CityList from "./components/CityList";
+import "./App.css";
 
 function App() {
   return (
-    <>
-      <div>TEST</div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<CityList />} />
+          <Route path="cities" element={<CityList />} />
+          <Route path="countries" element={<p>Countryes</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
